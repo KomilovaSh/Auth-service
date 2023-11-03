49967a7112698cbbb008833d481592a70891e0eb
KomilovaSh <145709500+KomilovaSh@users.noreply.github.com>
2023-11-03 23:37:39 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// const wrapper=document.querySelector('.wrapper');
+// const loginLink=document.querySelector('.login-link');
+// const registerLink=document.querySelector('register-link');
+
+
+// loginLink.addEventListener('click', () =>{
+//     wrapper.classList.add('active');
+// });
+
+// btnPopup.addEventListener('click', ()=>{
+//     wrapper.classList.add('active=popup')
+// });
